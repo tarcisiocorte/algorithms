ef1d9d375d8d0e949e7f2140c3077cd99cdff8f8
Tarcisio Costa <tarcisiocorte@gmail.com>
2021-02-18 11:23:52 -0300

    feat: add Binary Search in a sorted array

---
+++ Tccp.Algorithm/Arrays/BinarySerach.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tccp.Algorithm.Arrays
+{
+    //Binary search in a sorted array.
+    public static class BinarySerach
+    {
+       public static int BinarySearch(int[] array, int value)
+        {
+            var size = array.Length;
+            int middle;
+            int lower = 0;
+            int higher = size - 1;
+            while (lower <= higher)
+            {
+                middle = lower + (higher - lower) / 2;
+                if(array[middle] == value)
+                {
+                    return middle;
+                }
+                else if(array[middle] < value)
+                {
+                    lower = middle + 1;
+                }
+                else
+                {
+                    higher = middle - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Tccp.Algorithm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Tccp.Algorithm.Arrays;
+
+namespace Tccp.Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySerach.BinarySearch(new int[] { 10, 12, 20, 28, 29, 45, 48, 50 }, 19);
+        }
+    }
+}
