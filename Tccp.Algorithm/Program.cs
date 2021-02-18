37084fda769e884562862386d9af33df453940e2
Tarcisio Costa <tarcisiocorte@gmail.com>
2021-02-18 14:32:19 -0300

    feat: MovingTotal exercise

--- Tccp.Algorithm/Program.cs
+++ Tccp.Algorithm/Program.cs
@@ -7,7 +7,33 @@ namespace Tccp.Algorithm
     {
         static void Main(string[] args)
         {
+            ExecuteMovingTotal();
+        }
+
+        #region Samples code to execute in the Main program
+        public void ExecuteBinarySearch()
+        {
             BinarySerach.BinarySearch(new int[] { 10, 12, 20, 28, 29, 45, 48, 50 }, 19);
         }
+
+        public static void ExecuteMovingTotal()
+        {
+            MovingTotal movingTotal = new MovingTotal();
+
+            movingTotal.Append(new int[] { 1, 2, 3, 4 });
+
+            Console.WriteLine(movingTotal.Contains(6));
+            Console.WriteLine(movingTotal.Contains(9));
+            Console.WriteLine(movingTotal.Contains(12));
+            Console.WriteLine(movingTotal.Contains(7));
+
+            movingTotal.Append(new int[] { 5 });
+
+            Console.WriteLine(movingTotal.Contains(6));
+            Console.WriteLine(movingTotal.Contains(9));
+            Console.WriteLine(movingTotal.Contains(12));
+            Console.WriteLine(movingTotal.Contains(7));
+        }
+        #endregion
     }
 }
