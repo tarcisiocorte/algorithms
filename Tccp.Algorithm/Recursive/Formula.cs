fef9d1dbe5bc6ac82aa57c5e6657af5f1f92dab3
Tarcisio Costa <tarcisiocorte@gmail.com>
2021-02-19 08:39:19 -0300

    feat: add Formula factorial

---
+++ Tccp.Algorithm/Recursive/Formula.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tccp.Algorithm.Recursive
+{
+    public static class Formula
+    {
+        public static int Factorial(int number)
+        {
+            if (number <= 1) // Termination Condition
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1); // Body, Recursive Expansion
+        }
+    }
+}
